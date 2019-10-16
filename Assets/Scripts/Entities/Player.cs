89c4e98e527e94c08200122a8f1fe5d329fb41c0
AsylumRunner479 <54158974+AsylumRunner479@users.noreply.github.com>
2019-10-16 15:59:35 +1100

    initial commit

---
+++ Assets/Scripts/Entities/Player.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Rigidbody2D rigid;
+    public float startSize = 1f;
+    public float startSpeed = 5f;
+    public float sizemodifier = 0.1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+
+        
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.name.Contains("Food"))
+        {
+            Grow();
+            Destroy(other.gameObject);
+        }
+    }
+    void Move()
+    {
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 movement = Vector2.MoveTowards(transform.position, mousePos, startSpeed * Time.deltaTime);
+        rigid.MovePosition(movement);
+    }
+    public void Grow()
+    {
+        transform.localScale += new Vector3(sizemodifier, sizemodifier, sizemodifier);
+        Game.Instance.cam.orthographicSize += sizemodifier * 5f;
+    }
+    public void Scale(float size)
+    {
+        transform.localScale = (new Vector3(size, size, size));
+    }
+}
---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour
+{
+    #region Singleton
+    public static Game Instance = null;
+    private void Awake()
+    {
+        Instance = this;
+    }
+    #endregion
+
+    
+ public Camera cam;
+    public Renderer background;
+    // Start is called before the first frame update
+    public Vector3 GetAdjustedPosition(Vector3 incomingPos)
+    {
+        Vector3 size = background.bounds.size;
+        Vector3 halfSize = size * .5f;
+        if (incomingPos.x > halfSize.x)
+            incomingPos.x = halfSize.x;
+        if (incomingPos.y > halfSize.y)
+            incomingPos.y = halfSize.y;
+
+        if (incomingPos.x < -halfSize.x)
+            incomingPos.x = -halfSize.x;
+        if (incomingPos.y < -halfSize.y)
+            incomingPos.y = -halfSize.y;
+        return incomingPos;
+    }
+    public Vector3 GetRandomPosition()
+    {
+        Vector3 size = background.bounds.size;
+        Vector3 halfSize = size * .5f;
+        float x = Random.Range(-1f, 1f) * halfSize.x;
+        float y = Random.Range(-1f, 1f) * halfSize.y;
+        return new Vector3(x, y);
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
